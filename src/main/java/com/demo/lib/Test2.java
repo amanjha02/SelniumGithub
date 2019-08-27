0372f1524566cb86deabffd49daac007d473bfe9
Aman Kumar Jha <ajha@hsdyn.com>
2019-08-27 13:48:22 +0530

    one more print statement is added

--- src/main/java/com/demo/lib/Test2.java
+++ src/main/java/com/demo/lib/Test2.java
@@ -2,6 +2,7 @@ package com.demo.lib;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("lib package");
+		System.out.println("lib package1233");
+		System.out.println("lib package12333443");
 	}
 }
